1b839897f5b8011c83b5d93c4accf7fd5450e073
constantbourgois <constantbourgois@gmail.com>
2022-12-09 20:32:09 +0100

    firstcommit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it is working
+
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const publicPath = path.join(__dirname, '..', 'build');
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
