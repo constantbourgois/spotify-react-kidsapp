d0ffe6a685c8dc44071c1b74e309633ce5f209c3
constantbourgois <constantbourgois@gmail.com>
2022-12-11 19:45:07 +0100

    aze

--- server/index.js
+++ server/index.js
@@ -3,10 +3,16 @@ const express = require('express'); //Line 1
 const app = express(); //Line 2
 const port = process.env.PORT || 5000; //Line 3
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+const envir = process.env.NODE_ENV;
+
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
 // create a GET route
 app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+  res.send({ express: `YOUR EXPRESS BACKEND IS CONNECTED TO REACT ${envir} `}); //Line 10
 }); //Line 11
