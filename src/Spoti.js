53f9c64006a1af97c416a9736e80aff1af6d6d90
constantbourgois <constantbourgois@gmail.com>
2025-09-11 10:50:49 +0200

    addreccobeats

--- src/Spoti.js
+++ src/Spoti.js
@@ -10,96 +10,69 @@ export async function getToken() {
             'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
             'Content-Type': 'application/x-www-form-urlencoded'
         }
-    }).then(function (resp) {
-        // Return the response as JSON
-        return resp.json();
-
-    }).then(function (data) {
-
-        return data.access_token;
-
-    }).catch(function (err) {
-
-        // Log any errors
-        console.log('something went wrong', err);
-
-    });
+    }).then(resp => resp.json())
+      .then(data => data.access_token)
+      .catch(err => {
+          console.log('something went wrong', err);
+      });
 
     return Token;
 }
 
 function getRandomSearch() {
-    // A list of all characters that can be chosen.
     const characters = 'abcdefghijklmnopqrstuvwxyz';
-
-    // Gets a random character from the characters string.
     const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
-    let randomSearch = '';
-
-    // Places the wildcard character at the beginning, or both beginning and end, randomly.
-
-    randomSearch = '%25' + randomCharacter + '%25';
-
-    return randomSearch;
+    return '%25' + randomCharacter + '%25'; // %25 = URL encoded "%"
 }
 
 export async function fetchTracks(token) {
-
-
-    //const token = await getToken();
-    //const randomOffset = Math.floor(Math.random() * 1000);
     const randomOffset = 0;
     const q = getRandomSearch();
 
-    var myHeaders = new Headers();
-    console.log(token);
+    const myHeaders = new Headers();
     myHeaders.append("Authorization", `Bearer ${token}`);
 
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        //redirect: 'follow'
-    };
+    const requestOptions = { method: 'GET', headers: myHeaders };
 
-
-
-    const Songs = await (await fetch(`https://api.spotify.com/v1/search?q=${q}&offset=${randomOffset}&limit=50&type=track`, requestOptions)).json();
+    // Search for tracks on Spotify
+    const Songs = await (await fetch(
+        `https://api.spotify.com/v1/search?q=${q}&offset=${randomOffset}&limit=50&type=track`,
+        requestOptions
+    )).json();
 
     const tracks = Songs["tracks"]["items"];
 
-    async function getTracks() {
+    // Helper: fetch audio features from Reccobeats
+    async function fetchAudioFeatures(track) {
+        const query = encodeURIComponent(`${track.name} ${track.artists[0].name}`);
+        try {
+            const response = await fetch(`https://api.reccobeats.com/audio-features?query=${query}`);
+            if (!response.ok) throw new Error("Reccobeats fetch failed");
+            const audio_features = await response.json();
 
-        await Promise.all(tracks.map(async (track) => {
-
-            await fetch(`https://api.spotify.com/v1/audio-features/${track['id']}`, requestOptions).then(
-                function (response) {
-                    return response.json()
-                })
-                .then(
-                    function (audio_features) {
-                        track.audio_features = audio_features;
-                        track.valence = audio_features['valence'];
-                        track.danceability = audio_features['danceability'];
-                        track.energy = audio_features['energy'];
-
-                    })
-                .catch(error => console.log('error', error));
-
-        }))
-        return tracks;
+            // Map values back into track object
+            track.audio_features = audio_features;
+            track.valence = audio_features['valence'] || 0;
+            track.danceability = audio_features['danceability'] || 0;
+            track.energy = audio_features['energy'] || 0;
+        } catch (error) {
+            console.error("Reccobeats error:", error);
+            track.audio_features = {};
+            track.valence = 0;
+            track.danceability = 0;
+            track.energy = 0;
+        }
     }
-    getTracks();
-    console.log(tracks.length);
 
+    // Attach audio features to each track
+    await Promise.all(tracks.map(track => fetchAudioFeatures(track)));
 
-    return tracks
-
-
+    console.log("Tracks fetched:", tracks.length);
+    return tracks;
 }
 
 export async function getTrack(Songs, mood) {
     let songsArray = Songs;
-
     let songUrl;
 
     if (songsArray && mood) {
@@ -107,22 +80,21 @@ export async function getTrack(Songs, mood) {
         songsArray = songsArray.sort((a, b) => a.valence - b.valence);
 
         switch (mood) {
-            case "happy": songUrl = songsArray[Math.floor((songsArray.length) / 2)]['preview_url']; return songUrl
-            case "sad": songUrl = songsArray[0]['preview_url']; return songUrl
+            case "happy":
+                songUrl = songsArray[Math.floor(songsArray.length / 2)]['preview_url'];
+                return songUrl;
+            case "sad":
+                songUrl = songsArray[0]['preview_url'];
+                return songUrl;
             case "excited":
                 // get the song with highest danceability
                 const dancingSong = songsArray.reduce((previous, current) => {
-                    let a = current.danceability > previous.danceability ? current : previous;
-                    return a;
+                    return current.danceability > previous.danceability ? current : previous;
                 });
                 songUrl = dancingSong['preview_url'];
-                return songUrl
-            default: return songUrl
+                return songUrl;
+            default:
+                return songUrl;
         }
     }
-
 }
-
-
-
-
